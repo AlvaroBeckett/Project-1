96a05b0e059c985271b8b40f89ec1f0029d0fa51
Unknown <julia.shreckhise@gmail.com>
2018-03-25 14:21:17 -0400

    working on database pushing

--- assets/javascript/map2.js
+++ assets/javascript/map2.js
@@ -19,9 +19,6 @@ var config = {
 // };
 
 firebase.initializeApp(config);
-var database = firebase.database();
-
-var barName = "";
 
 var map;
 var infoWindow;
@@ -113,7 +110,7 @@ function initMap() {
                                     Open? | ${place.vicinity} | <a href="${place.website}" target="_blank">website</a>
                                 </div>
                                 <div>
-                                <button class="rate" data-name="${place.name} data-id=${place.place_id}">Rate</button> 
+                                <button class="rate" data-name="${place.name}" data-id="${place.place_id}">Rate</button> 
                                 </div>`
                             );
                         }
@@ -140,33 +137,27 @@ function initMap() {
 
 
 //===================== Modal Stuff
-//get random bar name for testing
-var barNames = ["bar1", "bar2", "bar3"];
-var index = Math.floor(Math.random() * barNames.length);
-var bar = barNames[index];
-$("#bar-name").html(bar);
 
 var ratings =[
     {question: "Guy/Girl Ratio",
-    answerChoices: ["More Guys", "More Girls", "EqualRatio"],
+    answerChoices: ["More Guys", "More Gals", "Equal Ratio"],
     },
     {question: "Atmosphere",
-    answerChoices: ["Dead", "Chill", "Interactive", "Party"],
+    answerChoices: ["Dead", "Chill", "Inviting", "Epic"],
     },
     {question: "Cleanliness",
     answerChoices: ["Gross", "Eh...", "Clean"],
     },
 ];
 
-
-///for some reaosn this is only working on the button in the html already
-$(".rate").on("click", function() {
+$(document).on("click",".rate", function(){
     $("#myModal").css("display", "block");
+    var barName = $(this).data("name");
+    var placeID = $(this).data("id");
+    console.log("should be id: " + placeID);
 
-    console.log($(this).data("name"));
-    console.log($(this).data("id"));
-
-
+    $("form").empty();
+    $("#bar-name").html(barName);
 
 //displaying questions and answer choices
 //display question
@@ -198,4 +189,50 @@ $(".close").on ("click", function () {
     $("#myModal").css("display", "none");
 });
 
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++
+//Getting new rating into firebase
+var database = firebase.database();
+database.ref().on("value", function(snapshot) {
+    console.log(snapshot.val());
+    snapshot.forEach(function(child) {
+        console.log(child.key+": "+ child.val());
+      });
+});
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    //Grab selections
+    for (var i = 0; i < ratings.length; i++) {
+        console.log("Selected:" + $(`input:radio[name="${ratings[i].question}"]:checked`).val());
+        //grab answer choices
+        for (var j = 0; j < ratings[i].answerChoices.length; j++) {
+        }
+    }
+
+
+    var newR1 = r1; //(r1 + storedR1)/numberRatings;
+    var newR2 = r2;//(r2 + storedR2)/numberRatings;
+    var newR3 = r3;//(r3 + storedR3)/numberRatings;
+
+    var newRating = {
+        name: $("#bar-name").val(),
+        R1: newR1,
+        R2: newR2,
+        R3: newR3, //add timestamp?
+    };
+
+    console.log(newRating)
+    database.ref().push(newRating);
+    //clear inputs
+    $("input").val("");
+
+    $("#r1avg").empty().html(newR1);
+    $("#r2avg").empty().html(newR2);
+    $("#r3avg").empty().html(newR3);
+});
 
