320de83c942e840a58e13dd116f77c6d491a5804
Unknown <julia.shreckhise@gmail.com>
2018-03-26 20:30:48 -0400

    Auto stash before merge of "Julia" and "master"
    database work in rate.js

--- assets/javascript/map2.js
+++ assets/javascript/map2.js
@@ -242,15 +242,13 @@ database.ref().on("value", function(snapshot) {
 $("#submit").on("click", function (event) {
    event.preventDefault();
 
-   //Grab selections
-   for (var i = 0; i < ratings.length; i++) {  
-   console.log("Selected:" + $(`input:radio[name="${ratings[i].question}"]:checked`).val());
-   //grab answer choices
-   for (var j = 0; j < ratings[i].answerChoices.length; j++) {
-
-   
-   }
-   }
+    //Grab selections
+    for (var i = 0; i < ratings.length; i++) {
+        console.log("Selected:" + $(`input:radio[name="${ratings[i].question}"]:checked`).val());
+        //grab answer choices
+        for (var j = 0; j < ratings[i].answerChoices.length; j++) {
+        }
+    }
 
 
    var newR1 = r1; //(r1 + storedR1)/numberRatings;
@@ -258,20 +256,13 @@ $("#submit").on("click", function (event) {
    var newR3 = r3;//(r3 + storedR3)/numberRatings;
 
    var newRating = {
-       name: $("#bar-name").val(),
-       R1: newR1,
-       R2: newR2,
-       R3: newR3, //add timestamp?
-   };
+    name: barName,
+    R1: { moreGuys: 0, moreGals: 0, EqualRatio: 0 },
+    R2: { Dead: 0, Chill: 0, Inviting: 0, Epic: 0 },
+    R3: { Gross: 0, Eh: 0, Clean: 0 }
+    };
 
-   console.log(newRating)
-   database.ref().push(newRating);
-   //clear inputs
-   $("input").val("");
-
-   $("#r1avg").empty().html(newR1);
-   $("#r2avg").empty().html(newR2);
-   $("#r3avg").empty().html(newR3);
-
+    console.log("new rating " + newRating)
+    database.ref().child(placeID).set(newRating);
 
 });
